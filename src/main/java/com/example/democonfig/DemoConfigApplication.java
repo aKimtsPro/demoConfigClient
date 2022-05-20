a6d34b4d1753e45bca69805cc8f4ed06850f2c95
akimtspro <alexandre.kimtsaris@outlook.com>
2022-05-20 14:24:41 +0200

    post demo + json auto-process + commented + L + ratio

--- src/main/java/com/example/democonfig/DemoConfigApplication.java
+++ src/main/java/com/example/democonfig/DemoConfigApplication.java
@@ -1,5 +1,6 @@
 package com.example.democonfig;
 
+import com.example.democonfig.model.Personne;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 @Slf4j
 @SpringBootApplication
@@ -17,16 +19,26 @@ public class DemoConfigApplication {
     }
 
     @Bean
-    public Function<String,String> processor(){
+    // output-out-0
+    public Supplier<Personne> output(){
+        return () -> new Personne("dubois","luc");
+    }
+
+    @Bean
+    // processor-in-0
+    // processor-out-0
+    public Function<Personne,Personne> processor(){
         return (in) ->{
             log.info("PROCESSING: " + in);
-            return in+ " PROCESSED";
+            in.setNom("PROCESSED: " + in.getNom());
+            return in;
         };
     }
 
     @Bean
+    // input-in-0
     public Consumer<String> input(){
-        return (message) -> log.info("RECIEVED: " + message);
+        return (personne) -> log.info("RECIEVED: " + personne);
     }
 
 }
---
+++ src/main/java/com/example/democonfig/model/Personne.java
@@ -0,0 +1,15 @@
+package com.example.democonfig.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Personne {
+
+    private String nom;
+    private String prenom;
+
+}
